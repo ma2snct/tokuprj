26f31200a68126fb0e35af524bbb941e9a8b90dc
ma2snct <knightmarespark@gmail.com>
2015-11-15 16:50:39 +0900

    ファイル名をpostでとれた

--- routes/files.js
+++ routes/files.js
@@ -6,10 +6,23 @@ var fs = require('fs');
 //ルーティングで/filesに入ってきているのは前提
 //それ以降のurlを
 router.get('/', function(req, res, next) {
+  /*
   fs.readFile('./public/static/sample.csv', 'utf-8', function(err, text){
     console.log(text);
   });
+  */
   res.render('files', {title:'File_io'});
+});
+
+router.post('/add', function(req,res, next){
+  console.log(req.body.file/*files.filefile.path*/);
+  /*
+  fs.readFile(req.body.file, 'utf-8', function(err, text){
+    console.log(text);
+  });
+  */
+  res.render('files', {title:'File_io add'});
+
 });
 
 module.exports = router;
