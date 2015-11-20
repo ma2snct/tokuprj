2b27aecddd5f3d2c8838e3e12b61cfe6bccdf88c
ma2snct <knightmarespark@gmail.com>
2015-11-20 14:15:39 +0900

    added a new textissue 01

--- routes/files.js
+++ routes/files.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 var fs = require('fs');
 var formidable = require("formidable");
-var nano = require('nano')('http://localhost:5984');
-var db = require('nano')('http://localhost:5984/sotsu');
-var alice = nano.use('sotsu');
 
 //ルーティングで/filesに入ってきているのは前提
 //それ以降のurlを
@@ -18,28 +15,24 @@ router.get('/', function(req, res, next) {
   res.render('files', {title:'File_io'});
 });
 
-//Error: ENOENT, open でいかれ 11/16
+//Error: ENOENT, open でいかれ 11/16 can't find uploaded file.
 router.post('/add', function(req,res, next){
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files){
     fs.rename(files.upload.path, "/tmp/test.csv", function(err){
-      if(err) console.log('input err' + err);
+      if(err){
+        console.log('input err' + err);
+      }else{
+        console.log('input :'+files.upload.path);
+      }
     });
   });
-//ファイルオープン
 
+  //file open
   fs.readFile("/tmp/test.csv", 'utf-8', function(err, file) {
     if(err) console.log('output err' + err);
     else console.log('done\n'+file);
   });
-
-  //CouchDB
-  db.insert({"foo":"baz"}, "ryujidoc", function(err, foo){
-	   if(!err)
-		   console.log("it worked");
-	  else
-		  console.log("sad panda");
-    });
   res.render('files', {title:'File_io add'});
 
 });
