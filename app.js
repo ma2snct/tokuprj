64925df25c0a305f608e22ec30bdb03ce84ce210
ma2snct <knightmarespark@gmail.com>
2015-11-27 15:57:05 +0900

    express3に変更、ファイルアップロード機能そのまま

--- app.js
+++ app.js
@@ -1,72 +1,55 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-
-var routes = require('./routes/index');// /
-var users = require('./routes/users');
-var files = require('./routes/files');// /files
-
-
-//added modules
-//for CouchDB
-var nano = require('nano');
-var fs = require('fs');
-
-
+var express = require('express')
+  ,multer = require('multer');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(multer({dest:'uploads/'}));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/files', files);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(req, res){
+  res.send('Hello, World');
 });
 
-// error handlers
+//------
+//to upload file
+var multer  = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        //cb(null, '/tmp/my-uploads')
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now())
+  }
+})
+var upload = multer({ storage: storage })
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+app.get('/file', function(req, res, next) {
+  res.render('files', {title:'File_io'});
+});
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+//Error: ENOENT, open 11/16 can't find uploaded file.
+//-> must make folder before run
+app.post('/add', upload.single('avatar'), function (req, res, next){
+  console.log(req.file)
+  res.render('files', {title:'File_io add'});
+  res.status(204).end();
+});
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  //app.engine('ejs', require('ejs-locals'));
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + 'public'));
+  //app.use(express.static(path.join(__dirname + 'public')));
 });
 
 
-module.exports = app;
+
+app.listen(3000, function(){
+  console.log("Express server listening on http://localhost:3000");
+});
