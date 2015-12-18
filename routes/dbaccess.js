65e31b38ebd6709609fd7d56ee3f4b7882b765e8
ma2snct <knightmarespark@gmail.com>
2015-12-18 15:54:36 +0900

    parsehl7完成

--- routes/dbaccess.js
+++ routes/dbaccess.js
@@ -13,6 +13,32 @@ var toDoubleDigits = function(num){
   return num;
 }
 
+//CouchDBにドキュメントとして入れる
+//引数は患者の名前、データがはいった変数
+function dbInsert(name, in_data){
+  sotsu.get(name, function(err, body, header){
+    if(!err){
+      doc=body;
+
+      var dt = new Date();
+      var m = toDoubleDigits(dt.getMonth()+1);
+      var d = toDoubleDigits(dt.getDate());
+
+      //ドキュメント名は患者の名前に日付が追加される
+      //sotsu.insert({_id:name+m+d, data:in_data}, function(err, body, header){
+      //デバッグ用　
+      sotsu.insert({_id:'000', data:in_data}, function(err, body, header){
+        if(!err){
+          //res.send(body._id);
+        }else{
+          console.log('err:' + err);
+        }
+      });
+    }
+  });
+}
+
+
 //読み込むファイルがCSVのときにつかう
 var csv = require('ya-csv');
 var reader = csv.createCsvFileReader('./public/static/HEM_201411.csv', {
@@ -26,8 +52,6 @@ var reader = csv.createCsvFileReader('./public/static/HEM_201411.csv', {
 //検査データのときに使う
 exports.parse = function(req, res, next) {
 
-  //console.log(reader);
-
   var in_data={};
   reader.addListener('data', function (data) {
     //todo data[1]のほうを可変にする
@@ -36,24 +60,8 @@ exports.parse = function(req, res, next) {
 
   var doc;
 
-  sotsu.get(req.user.username, function(err, body, header){
-    if(!err){
-      doc=body;
+  dbInsert(req.user.username, in_data);
 
-      // making date
-      var dt = new Date();
-      var m = toDoubleDigits(dt.getMonth()+1);
-      var d = toDoubleDigits(dt.getDate());
-
-  	  sotsu.insert({_id:req.user.username+m+d, data:in_data}, function(err, body, header){
-        if(!err){
-          //res.send(body._id);
-        }else{
-    		  console.log('err:' + err);
-  		  }
-      });
-    }
-  });
   res.send('try to parse');
 };
 
@@ -64,6 +72,8 @@ exports.horizontialparse = function(req, res, next) {
 
   //読み込むファイルがCSVのときにつかう
   //todo これを関数の外で使えるようにする
+  //現状関数の中にいれとかないと空のドキュメントになる
+  /*
   var csv = require('ya-csv');
   var reader = csv.createCsvFileReader('./public/static/HEM_201411.csv', {
     'separator': ',',
@@ -71,6 +81,7 @@ exports.horizontialparse = function(req, res, next) {
     'escape': '"',
     'comment': '',
   });
+  */
 
 
   var in_data={};
@@ -94,31 +105,34 @@ exports.horizontialparse = function(req, res, next) {
 
   var doc;
 
-  sotsu.get('ryuji'/*req.user.username*/, function(err, body, header){
-    if(!err){
-      doc=body;
-
-      // making date
-      var dt = new Date();
-      var m = toDoubleDigits(dt.getMonth()+1);
-      var d = toDoubleDigits(dt.getDate());
-
-      console.log("---" + list);
-
-      for(var k in list){
-    	  sotsu.insert({_id:"ryuji-01"/*req.user.username*/+d+"-"+k, data:list[k]}, function(err, body, header){
-          if(!err){
-            //res.send(body._id);
-          }else{
-      		  console.log('err:' + err);
-    		  }
-        });
-      }
-    }
-  });
+  dbInsert(req.user.username, in_data);
 
   res.send('try to horizontial csv file');
 };
+
+var tags = {
+  //ここに足していけばHL7はよくなっていく
+  RXE: {
+    "RXE-0":"セグメントID",
+    "RXE-1":"数量/タイミング",
+    "RXE-2":"与薬コード",
+    "RXE-3":"与薬量－最小",
+    "RXE-4":"与薬量－最大",
+    "RXE-5":"与薬単位",
+    "RXE-6":"与薬剤形",
+    "RXE-7":"依頼者の投薬指示"
+  },
+
+  OBX: {
+    "OBX-0" : "セグメント ID",
+    "OBX-1" : "セット ID - OBX",
+    "OBX-2" : "値型",
+    "OBX-3" : "検査項目",
+    "OBX-4" : "検査副 ID",
+    "OBX-5" : "検査値",
+    "OBX-6" : "単位"
+  }
+}
 
 //to insert hl7 text file to CouchDB
 exports.parsehl7 = function(req, res, next){
@@ -128,65 +142,20 @@ exports.parsehl7 = function(req, res, next){
 
   var rl = readline.createInterface(rs, {});
 
-  //todo タグを充実させる
-  var tags = {"RXE":"薬剤/処置 コード化されたオーダ",
-			"NTE":"注釈・コメント",
-			"TQ1":"タイミング/数量"
-			};
-
-  //todo RXEタグ以外にも対応させる
-  var childtags = {
-    "RXE-0":"セグメントID",
-    "RXE-1":"数量/タイミング",
-    "RXE-2":"与薬コード",
-    "RXE-3":"与薬量－最小",
-    "RXE-4":"与薬量－最大",
-    "RXE-5":"与薬単位",
-    "RXE-6":"与薬剤形",
-    "RXE-7":"依頼者の投薬指示"
-  };
-
-  var OBXtags = {
-    "OBX-0" : "セグメント ID",
-    "OBX-1" : "セット ID - OBX",
-    "OBX-2" : "値型",
-    "OBX-3" : "検査項目",
-    "OBX-4" : "検査副 ID",
-    "OBX-5" : "検査値",
-    "OBX-6" : "単位"
-  }
-
-
-
   var in_data = {};
 
-  //whenever input line for RXE
-  /*
+  //
   rl.on('line', function(line){
-  	var tag = line.substr(0,3);
-    var i=0;
-    for(t in childtags){
-
-      var n=line.indexOf("|")
-
-      in_data[childtags[t]]=line.substr(0,n);
-      line = line.substr(n+1);
-      i++;
-
-      console.log(in_data);
-    }
-  });
-  */
-
-  //whenever input line for OBX
-  rl.on('line', function(line){
+    //入力からタグを判別する　このままだとただの文字列
     var tag = line.substr(0,3);
     var i=0;
-    for(t in OBXtags){
+
+    //そのタグ名のオブジェクトでループ
+    for(t in tags[tag]){
 
       var n=line.indexOf("|")
 
-      in_data[OBXtags[t]]=line.substr(0,n);
+      in_data[tags[tag][t]]=line.substr(0,n);
       line = line.substr(n+1);
       i++;
 
@@ -195,25 +164,7 @@ exports.parsehl7 = function(req, res, next){
   });
 
 
-
-  sotsu.get(req.user.username, function(err, body, header){
-    if(!err){
-      doc=body;
-
-      var dt = new Date();
-      var m = toDoubleDigits(dt.getMonth()+1);
-      var d = toDoubleDigits(dt.getDate());
-
-  	  sotsu.insert({_id:req.user.username+m+d, data:in_data}, function(err, body, header){
-        if(!err){
-          //res.send(body._id);
-        }else{
-    		  console.log('err:' + err);
-  		  }
-      });
-    }
-  });
-
+  dbInsert(req.user.username, in_data);
 
   res.send('hl7 text parse to json');
 
@@ -259,7 +210,7 @@ exports.getdb =  function(req, res, next) {
       console.log("---out---" + out);
       //res.send('ok');
       var list = ['zero', 'one', 'two'];
-      res.render('getdb', {out:out});
+      res.render('getdb', {list:list});
 
     }
   }
